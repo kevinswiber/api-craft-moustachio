002ae423f20285af5f26c6223b9f742318a69e9e
Kevin Swiber <kswiber@gmail.com>
2012-12-12 16:38:29 -0500

    Adding better tests.

--- api/spec/all.js
+++ api/spec/all.js
@@ -1,11 +1,14 @@
 var APIeasy = require('api-easy');
 var assert = require('assert');
 
-var suite = APIeasy.describe('localhost');
+var suite = APIeasy.describe('Moustach.io');
 
 suite
   .discuss('When selecting all moustaches')
   .use('localhost', 3111)
   .get('/moustaches')
-  .expect(200)
+    .expect(200)
+    .expect('should have a JSON content type', function(err, res) {
+      assert.include(res.headers['content-type'], 'application/json');
+    })
   .export(module);
